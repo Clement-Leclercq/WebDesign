97a9b360a5559bb31458dbf42afa5d2556aff424
Clément Leclercq <clement.leclercq@cpe.fr>
2020-10-20 12:20:17 +0200

    javascript en plus

--- JavaScript/Credentials/ScriptAuthentification.js
+++ JavaScript/Credentials/ScriptAuthentification.js
@@ -1,3 +1,22 @@
 // Authentification avec disparition du formulaire
+window.onload = function(){
+    var form = document.getElementById("formConn");
+    form.onsubmit = verifAuth;
+    verifConn();
+};
+function verifConn(){ // s'occupe de vérifier si l'utilisateur est co et transforme l'affichage
+    formConn.classList.remove("hide");
+    if (localStorage.getItem("Username") !== undefined){
+        divConn.classList.remove("hide");
+        formConn.classList.add("hide");
+    }
+}
+function verifAuth(){ //  s'occupe de vérifier le formulaire
+    var username = document.getElementById("usrname1").value;
+    var password = document.getElementById("pword1").value;
+    if (Users[username] !== undefined && Users[username].password === password){
+        localStorage.setItem('Username',username);
+    }
+}
 
-// Cookie pou rester connecté
+
--- JavaScript/Credentials/ScriptRetrieveData.js
+++
@@ -1,5 +0,0 @@
-// JSON récupérer les données:
-    // - pour d'authentifier -> appeler ScriptAuthentification
-    // - pour s'inscrire -> appeler ScriptInscription
-
-
