8a38cbf77aa4e62532fbbb1481e87ea3fc803bb0
PierreCPE <pierre.gosson@cpe.fr>
2020-11-22 16:34:34 +0100

    Index JS

--- JavaScript/Index.js
+++ JavaScript/Index.js
@@ -5,11 +5,15 @@ if ("content" in document.createElement("template")) {
     for (let i = 0; i < length(Destinations)+1; i++) {
         var template =document.querySelector("#tableIm")
 
+        
         var box = document.querySelector("box");
         var clone = document.importNode(template.content, true);
         var p = clone.querySelectorAll("p");
-        p.textContent = Destinations.afficher() ;
-        Destinations[1].textContent = "Stuff";
+        var img = clone.querySelectorAll("img)")
+        img.src = Destinations[0+i].getImage() ;
+        img.alt = Destinations[0+i]._ville ;
+        p.textContent = Destinations[0+i].afficher() ;
+        ;
 
         box.appendChild(clone);
 
@@ -18,8 +22,10 @@ if ("content" in document.createElement("template")) {
 }
 
     else {
-        alert("Votre navigateur ne support pas ce site internet, essayer des navigateurs plus récents")
+        alert("Votre navigateur n'est pas compatible avec ce site internet, merci d'essayer des navigateurs plus récents")
 }
+
+
 //En s'aidant du mdn template premiere ligne de l'exmple du tableau
 //Faire une boucle for pour les 12 voyages qui créent des clones qui avec querySelector
 //Selectionne par défaut des balise pour les remplir (voir comment remplir l'intérieur)
