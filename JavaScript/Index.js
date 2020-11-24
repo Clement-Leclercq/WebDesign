ea8d20ffea548b1ce445f2f260f9f78d7ccf9484
PierreCPE <pierre.gosson@cpe.fr>
2020-11-24 09:03:31 +0100

    JS searchbar+template

--- JavaScript/Index.js
+++ JavaScript/Index.js
@@ -1,5 +1,19 @@
 const tripList = document.getElementById('tripList');
+const searchBar = document.getElementById('searchBar');
 let websiteTrips = [];
+
+searchBar.addEventListener('keyup', (e) => {
+    const searchString = e.target.value.toLowerCase();
+
+    const filteredTrips = websiteTrips.filter((Destination) => {
+        return (
+            Destination._pays.toLowerCase().includes(searchString) ||
+            Destination._ville.toLowerCase().includes(searchString)
+        );
+    });
+    displayTrips(filteredTrips);
+});
+
 
 const loadTrips = async () => {
     try {
@@ -9,12 +23,12 @@ const loadTrips = async () => {
         console.error(err);
     }
 };
-
+// LE LOADING NE MARCHE PAS 
 const displayTrips = (trips) => {
     const htmlString = trips
-        .map((voyage) => {
+        .map((Destination) => {
             return `
-            <li class="voyage">
+            <li class="Destination">
                 <p>${Destination.afficher}</p>
                 
                 <img src="${Destination.getImage}"></img>
