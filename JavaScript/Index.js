f131b71d98b6ec63af8da2697db066d2bc923f68
PierreCPE <pierre.gosson@cpe.fr>
2020-12-06 17:14:16 +0100

    m

--- JavaScript/Index.js
+++ JavaScript/Index.js
@@ -1,51 +1,3 @@
-// Code de la search bar et du loading d'image sur l'index
-
-
-
-const tripList = document.getElementById('tripList');
-const searchBar = document.getElementById('searchBar');
-let websiteTrips = [];
-
-searchBar.addEventListener('keyup', (e) => {
-    const searchString = e.target.value.toLowerCase();
-
-    const filteredTrips = websiteTrips.filter((Destination) => {
-        return (
-            Destination._pays.toLowerCase().includes(searchString) ||
-            Destination._ville.toLowerCase().includes(searchString)
-        );
-    });
-    displayTrips(filteredTrips);
-});
-
-
-const loadTrips = async () => {
-    try {
-        websiteTrips = Destinations;
-        displayTrips(websiteTrips);
-    } catch (err) {
-        console.error(err);
-    }
-};
-// LE LOADING NE MARCHE PAS 
-const displayTrips = (trips) => {
-    const htmlString = trips
-        .map((Destination) => {
-            return `
-            <li class="Destination">
-                <p>${Destination.afficher}</p>
-                
-                <img src="${Destination.getImage}"></img>
-            </li>
-        `;
-        })
-        .join('');
-    tripList.innerHTML = htmlString;
-};
-
-loadTrips();
-
-
 //Liste de destination :
 
 let Destinations = [
@@ -170,3 +122,48 @@ let Destinations = [
   
   
   ];
+
+// Code de la search bar et du loading d'image sur l'index
+
+
+
+const tripList = document.getElementById('tripList');
+const searchBar = document.getElementById('searchBar');
+let websiteTrips = [];
+
+searchBar.addEventListener('keyup', (e) => {
+    const searchString = e.target.value.toLowerCase();
+
+    const filteredTrips = websiteTrips.filter((Destination) => {
+        return (
+            Destination._pays.toLowerCase().includes(searchString) ||
+            Destination._ville.toLowerCase().includes(searchString)
+        );
+    });
+    displayTrips(filteredTrips);
+});
+
+
+const loadTrips = async () => {
+    displayTrips(Destinations);
+    
+};
+// LE LOADING NE MARCHE PAS 
+const displayTrips = (trips) => {
+    const htmlString = trips
+        .map((Destination) => {
+            return `
+            <li class="Destination">
+                <p>${Destination.afficher}</p>
+                
+                <img src="${Destination.getImage}"></img>
+            </li>
+        `;
+        })
+        .join('');
+    tripList.innerHTML = htmlString;
+};
+
+loadTrips();
+
+
