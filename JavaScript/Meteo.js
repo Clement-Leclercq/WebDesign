6cafb2c2d8720888b709501fff99e3347f24a28a
PierreCPE <pierre.gosson@cpe.fr>
2020-12-08 08:23:50 +0100

    Api weather

--- JavaScript/Meteo.js
+++ JavaScript/Meteo.js
@@ -1,4 +1,8 @@
   
+//API WEATHER POUR LA METEO (Tananarive ne semble pas etre connu sur l'api weather)
+
+
+
 for (let i=0; i <= trips.length-1; i++) {
 
     window.fetch("https://api.openweathermap.org/data/2.5/weather?q="+trips[i].tag.replace("_"," ")+"&units=metric&lang=fr&appid=0fe9ac6e8cd825bf25243c32815ad2a8")
